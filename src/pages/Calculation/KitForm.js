ba308b960f9b4a9876e6379c50178dce4298e8a4
kuris996 <kuris_a@mail.ru>
2020-04-05 00:34:53 +0300

    kit uses standart upload

--- src/pages/Calculation/KitForm.js
+++ src/pages/Calculation/KitForm.js
@@ -27,52 +27,28 @@ const fieldLabels = {
 }
 
 class DraggerWrapper extends PureComponent {
-    state = { fileList: [] }
+    state = { currentFile: "" }
     render() {
-        const { fileList } = this.state
         const { children, uuid } = this.props
         const props = {
-            customRequest: _file => {
-                const formData = new FormData();
-
-                const { file } = _file
-
-                const fileName = getInputsPath(uuid) + file.name
-
-                formData.append('key', fileName)
-                formData.append('file', file);
-                
-                reqwest({
-                    url: bucketUrl,
-                    method: 'POST',
-                    processData: false,
-                    data: formData,
-                    success: () => {
-                        message.success('upload successfully.');
-                    },
-                    error: () => {
-                        message.error('upload failed.');
-                    },
-                });
+            action: bucketUrl,
+            data: { 
+                "key": this.state.currentFile,
             },
-            onRemove: file => {
-                this.setState(state => {
-                    const index = state.fileList.indexOf(file);
-                    const newFileList = state.fileList.slice();
-                    newFileList.splice(index, 1);
-                    return {
-                        fileList: newFileList,
-                    };
-                });
+            onChange(info) {
+                if (info.file.status === 'done') {
+                    message.success(`${info.file.name} успешно загружен.`);
+                } else if (info.file.status === 'error') {
+                    message.error(`${info.file.name} ошибка загрузки.`);
+                }
             },
             beforeUpload: file => {
                 this.setState(state => ({
-                    fileList: [...state.fileList, file],
+                    currentFile: getInputsPath(uuid) + file.name
                 }));
                 return true;
             },
-            fileList,
-        };
+        }
 
         return (
             <Dragger {...props}>
