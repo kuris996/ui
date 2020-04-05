33728dfe19506cecb3722f060e344cdce07f1551
kuris996 <kuris_a@mail.ru>
2020-04-06 01:05:00 +0300

    update

--- config/router.config.js
+++ config/router.config.js
@@ -3,21 +3,5 @@ export default [
     {
         path: '/',
         component: '../layouts/BasicLayout',
-        routes: [
-            { path: '/', redirect: '/calculation/task-list', },
-            // calculation
-            {
-                path: '/calculation',
-                name: 'Расчет',
-                icon: 'calculator',
-                routes: [
-                    {
-                        path: '/calculation/task-list',
-                        name: 'Задачи',
-                        component: './Calculation/TaskList',
-                    },
-                ],
-            },
-        ]
     },
 ]
