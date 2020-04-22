54a2bf0a3cb6570902222e260899b5eba69b4c2a
kuris996 <kuris_a@mail.ru>
2020-04-23 00:17:02 +0300

    theme switched to black

--- config/config.js
+++ config/config.js
@@ -25,6 +25,9 @@ export default {
     plugins,
     treeShaking: true,
     routes: pageRoutes,
+    theme: {
+        ...darkTheme,
+    },
     ignoreMomentLocale: true,
     lessLoaderOptions: {
         javascriptEnabled: true,
